81c19791c53a994dff6eda63257495c49b2fb291
Ossy-David <ossydavid3@gmail.com>
2026-09-11 10:00:37 +0000

    Add name

--- intro.js
+++ intro.js
@@ -1,3 +1,3 @@
-console.log("Ossy David Promise");
-console.log("Age:19");
+console.log("Name: Ossy David Promise");
+console.log("Age: 19");
 console.log("Favourite Programming Language: JavaScript");
